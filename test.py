2e7c416ee80ed376abea7d51f34d2c2d8f71ed8f
Senpai <pvnt20@gmail.com>
2022-11-25 12:29:02 +0530

    spaces

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from ok_xxx import OkXXXDownloader
 
-# # Example 1:
+# # Example 1 - Download lots of videos from page:
 # page_url = "https://ok.xxx/models/susy-gala/"
 # okxxx = OkXXXDownloader()
 # okxxx.download(page_url, page=True, parallel_download=12)
 
-# # Example 2:
+# # Example 2 - Download a single video:
 
 # okxxx = OkXXXDownloader()
 # okxxx.download(url="https://ok.xxx/video/103650/")
